5fb4cbff3bc049cbc317779c18f35b95fa4af089
Jayson Dela Cruz <jayson.p.delacruz@gmail.com>
2018-05-14 23:16:45 -0700

    Updated build.gradle and added dependency for Navigation Component.

--- app/src/main/java/com/gatheringhallstudios/mhworlddatabase/util/sqliteloader/adapters/FrameworkSQLiteProgram.java
+++ app/src/main/java/com/gatheringhallstudios/mhworlddatabase/util/sqliteloader/adapters/FrameworkSQLiteProgram.java
@@ -44,7 +44,7 @@ class FrameworkSQLiteProgram implements SupportSQLiteProgram {
     }
 
     @Override
-    public void close() throws Exception {
+    public void close() {
         mDelegate.close();
     }
 }
