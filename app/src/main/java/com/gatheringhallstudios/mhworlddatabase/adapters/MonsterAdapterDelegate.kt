22727a7b9b549416db053dcc6924cb8307f6808d
Carlos Fernandez <cfern1990@gmail.com>
2018-05-26 02:15:24 -0400

    MonsterAdapterDelegate no longer uses a custom view
    If we ever have need for separate custom views, we can separate them. Separate custom views require the use of the <merge> XML attribute for proper inflation.

--- app/src/main/java/com/gatheringhallstudios/mhworlddatabase/adapters/MonsterAdapterDelegate.kt
+++ app/src/main/java/com/gatheringhallstudios/mhworlddatabase/adapters/MonsterAdapterDelegate.kt
@@ -1,11 +1,8 @@
 package com.gatheringhallstudios.mhworlddatabase.adapters
 
-import android.content.Context
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 
 import com.gatheringhallstudios.mhworlddatabase.R
 import com.gatheringhallstudios.mhworlddatabase.common.SimpleListDelegate
@@ -14,34 +11,24 @@ import com.gatheringhallstudios.mhworlddatabase.getAssetDrawable
 
 import kotlinx.android.synthetic.main.listitem_monster.view.*
 
-/**
- * Creates a view object to represent a monster list item.
- */
-class MonsterListItemView(ctx: Context) : LinearLayout(ctx) {
-    init {
-        val inflater = LayoutInflater.from(ctx)
-        inflater.inflate(R.layout.listitem_monster, this, true)
-    }
-
-    fun setMonster(monster : MonsterView) {
-        val defaultIcon = R.drawable.question_mark_grey
-        val icon = context.getAssetDrawable(monster.data.icon, defaultIcon)
-        monster_icon.setImageDrawable(icon)
-        monster_name.text = monster.name
-    }
-}
-
 class MonsterAdapterDelegate(private val onSelected: (MonsterView) -> Unit)
-    : SimpleListDelegate<MonsterView, MonsterListItemView>() {
+    : SimpleListDelegate<MonsterView, View>() {
 
     override fun getDataClass() = MonsterView::class
 
     override fun onCreateView(parent: ViewGroup): View {
-        return MonsterListItemView(parent.context)
+        val inflater = LayoutInflater.from(parent.context)
+        return inflater.inflate(R.layout.listitem_monster, parent, false)
     }
 
-    override fun bindView(view: MonsterListItemView, data: MonsterView) {
-        view.setMonster(data)
+    override fun bindView(view: View, data: MonsterView) {
+        val ctx = view.context
+        val defaultIcon = R.drawable.question_mark_grey
+        val icon = ctx.getAssetDrawable(data.data.icon, defaultIcon)
+
+        view.monster_icon.setImageDrawable(icon)
+        view.monster_name.text = data.name
+
         view.setOnClickListener { onSelected(data) }
     }
 }
