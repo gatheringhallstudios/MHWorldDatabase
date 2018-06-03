911fb4acb8f42ad370d46daaa41392834f06bada
Carlos Fernandez <cfern1990@gmail.com>
2018-06-02 22:23:06 -0400

    MonsterViewModel loads breaks

--- app/src/main/java/com/gatheringhallstudios/mhworlddatabase/features/monsters/detail/MonsterDetailViewModel.kt
+++ app/src/main/java/com/gatheringhallstudios/mhworlddatabase/features/monsters/detail/MonsterDetailViewModel.kt
@@ -6,33 +6,25 @@ import android.arch.lifecycle.LiveData
 
 import com.gatheringhallstudios.mhworlddatabase.data.MHWDatabase
 import com.gatheringhallstudios.mhworlddatabase.data.dao.MonsterDao
-import com.gatheringhallstudios.mhworlddatabase.data.views.MonsterHabitatView
-import com.gatheringhallstudios.mhworlddatabase.data.views.MonsterHitzoneView
-import com.gatheringhallstudios.mhworlddatabase.data.views.MonsterRewardView
-import com.gatheringhallstudios.mhworlddatabase.data.views.MonsterView
+import com.gatheringhallstudios.mhworlddatabase.data.views.*
 
 /**
  * A ViewModel for any monster summary fragment
  */
 class MonsterDetailViewModel(application: Application) : AndroidViewModel(application) {
-    private val dao: MonsterDao
+    private val db = MHWDatabase.getDatabase(application)
+    private val dao = db.monsterDao()
 
-    init {
-        // todo: perhaps inject the database directly?
-        val db = MHWDatabase.getDatabase(application)
-        dao = db.monsterDao()
-    }
+    private var id: Int = -1
 
-    private var id: Int = 0
-
-    private var initialized = false
     lateinit var monster: LiveData<MonsterView>
     lateinit var habitats: LiveData<List<MonsterHabitatView>>
     lateinit var rewards: LiveData<List<MonsterRewardView>>
     lateinit var hitzones: LiveData<List<MonsterHitzoneView>>
+    lateinit var breaks: LiveData<List<MonsterBreakView>>
 
     fun setMonster(monsterId: Int) {
-        if (initialized) {
+        if (id == monsterId) {
             return
         }
 
@@ -42,7 +34,6 @@ class MonsterDetailViewModel(application: Application) : AndroidViewModel(applic
         habitats = dao.loadHabitats("en", monsterId)
         rewards = dao.loadRewards("en", monsterId)
         hitzones = dao.loadHitzones("en", monsterId)
-
-        initialized = true
+        breaks = dao.loadBreaks("en", monsterId)
     }
 }
