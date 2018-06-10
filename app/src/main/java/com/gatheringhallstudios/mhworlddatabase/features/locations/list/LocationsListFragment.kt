e4fec1ca0dcb08fa3845d63bc061a958d3d36ea1
Aaron <ahctang@gmail.com>
2018-06-10 20:03:04 +0900

    Fixing some strange merge conflicts

--- app/src/main/java/com/gatheringhallstudios/mhworlddatabase/features/locations/list/LocationsListFragment.kt
+++ app/src/main/java/com/gatheringhallstudios/mhworlddatabase/features/locations/list/LocationsListFragment.kt
@@ -8,20 +8,11 @@ import androidx.navigation.fragment.findNavController
 import com.gatheringhallstudios.mhworlddatabase.R
 import com.gatheringhallstudios.mhworlddatabase.adapters.BasicListDelegationAdapter
 import com.gatheringhallstudios.mhworlddatabase.adapters.LocationAdapterDelegate
-import com.gatheringhallstudios.mhworlddatabase.adapters.MonsterAdapterDelegate
 import com.gatheringhallstudios.mhworlddatabase.common.RecyclerViewFragment
 import com.gatheringhallstudios.mhworlddatabase.data.views.LocationView
-<<<<<<< HEAD
 import com.gatheringhallstudios.mhworlddatabase.features.locations.detail.LocationDetailPagerFragment
 import com.gatheringhallstudios.mhworlddatabase.util.BundleBuilder
 
-
-=======
-import com.gatheringhallstudios.mhworlddatabase.data.views.MonsterView
-import com.gatheringhallstudios.mhworlddatabase.features.monsters.MonsterDetailPagerFragment
-import com.gatheringhallstudios.mhworlddatabase.util.BundleBuilder
-
->>>>>>> f15fac5fe64b64d0e4b5fc135c385f3ff0033509
 /**
  * Fragment for a list of monsters
  */
@@ -43,27 +34,16 @@ class LocationsListFragment : RecyclerViewFragment() {
 
     // Setup adapter and navigation
     private val adapter = BasicListDelegationAdapter(LocationAdapterDelegate({
-<<<<<<< HEAD
         findNavController().navigate(
                 R.id.locationDetailDestination,
                 BundleBuilder().putInt(LocationDetailPagerFragment.ARG_LOCATION_ID, it.id).build()
         )
-=======
-//        findNavController().navigate(
-//                R.id.monsterDetailDestination,
-//                BundleBuilder().putInt(MonsterDetailPagerFragment.ARG_MONSTER_ID, it.id).build()
-//        )
->>>>>>> f15fac5fe64b64d0e4b5fc135c385f3ff0033509
     }))
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         this.setAdapter(adapter)
-
-<<<<<<< HEAD
         viewModel.getLocations()
-=======
-        viewModel.getLocations();
->>>>>>> f15fac5fe64b64d0e4b5fc135c385f3ff0033509
+
 
         viewModel.locations.observe(this, Observer<List<LocationView>>({
             adapter.items = it
