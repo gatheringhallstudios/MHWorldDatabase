7106657cd6ebf1fcbce1bb87fa107e441ff0b9b1
Aaron <ahctang@gmail.com>
2018-06-10 22:21:08 +0900

    Removed some erroneous code

--- app/src/main/java/com/gatheringhallstudios/mhworlddatabase/features/locations/detail/LocationGatheringListFragment.kt
+++ app/src/main/java/com/gatheringhallstudios/mhworlddatabase/features/locations/detail/LocationGatheringListFragment.kt
@@ -4,7 +4,9 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.gatheringhallstudios.mhworlddatabase.R
 import com.gatheringhallstudios.mhworlddatabase.adapters.BasicListDelegationAdapter
@@ -16,6 +18,7 @@ import com.gatheringhallstudios.mhworlddatabase.data.views.LocationView
 import com.gatheringhallstudios.mhworlddatabase.features.items.ItemDetailPagerFragment
 import com.gatheringhallstudios.mhworlddatabase.features.items.ItemListFragment
 import com.gatheringhallstudios.mhworlddatabase.util.BundleBuilder
+import kotlinx.android.synthetic.main.list_generic.*
 
 
 class LocationGatheringListFragment : Fragment() {
@@ -23,33 +26,31 @@ class LocationGatheringListFragment : Fragment() {
     companion object {
         private val ARG_LOCATION_ID = "LOCATION_ID"
 
-        @JvmStatic
-        fun newInstance(locationId: Int): ItemListFragment {
-            val f = ItemListFragment()
-            f.arguments = BundleBuilder().putSerializable(this.ARG_LOCATION_ID, locationId).build()
-            return f
-        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, parent: ViewGroup?, savedInstance: Bundle?): View? {
+        return inflater.inflate(R.layout.list_generic, parent, false)
     }
 
     private val viewModel by lazy {
         ViewModelProviders.of(parentFragment!!).get(LocationDetailViewModel::class.java)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private lateinit var adapter: BasicListDelegationAdapter<Any>
 
-        viewModel.locationItems.observe(this, Observer<List<LocationItemView>>({
-            adapter.items = it
-            adapter.notifyDataSetChanged()
-        }))
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+//        var locationItemsAdapterDelegate = LocationItemsAdapterDelegate(onSelected={
+//            findNavController().navigate(
+//                    R.id.itemDetailDestination,
+//                    BundleBuilder().putInt(ItemDetailPagerFragment.ARG_ITEM_ID, it.data.location_id).build())
+//        })
+//
+//        adapter = BasicListDelegationAdapter(locationItemsAdapterDelegate)
+//        //adapter.items = viewModel.locationItems.value
+//
+//        recycler_view.adapter = adapter
+
 
     }
-    // Setup adapter and navigation
-    private val adapter = BasicListDelegationAdapter(LocationItemsAdapterDelegate({
-        findNavController().navigate(
-                R.id.locationDetailDestination,
-                BundleBuilder().putInt(LocationDetailPagerFragment.ARG_LOCATION_ID, it.data.id).build()
-        )
-    }))
-
 }
