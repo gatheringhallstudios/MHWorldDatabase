f37b1e71c5a2f4139ba1710f0244c4e4858cd38b
Carlos Fernandez <cfern1990@gmail.com>
2018-06-22 18:09:21 -0400

    Monster Reward fragment now links to item

--- app/src/main/java/com/gatheringhallstudios/mhworlddatabase/features/monsters/detail/MonsterRewardFragment.kt
+++ app/src/main/java/com/gatheringhallstudios/mhworlddatabase/features/monsters/detail/MonsterRewardFragment.kt
@@ -8,22 +8,22 @@ import android.widget.Toast
 
 import com.gatheringhallstudios.mhworlddatabase.adapters.common.BasicListDelegationAdapter
 import com.gatheringhallstudios.mhworlddatabase.adapters.MonsterRewardAdapterDelegate
+import com.gatheringhallstudios.mhworlddatabase.adapters.common.CategoryAdapter
 import com.gatheringhallstudios.mhworlddatabase.adapters.common.SubHeaderAdapterDelegate
 import com.gatheringhallstudios.mhworlddatabase.common.RecyclerViewFragment
 import com.gatheringhallstudios.mhworlddatabase.adapters.common.SubHeader
 import com.gatheringhallstudios.mhworlddatabase.data.types.Rank
 import com.gatheringhallstudios.mhworlddatabase.data.views.MonsterRewardView
+import com.gatheringhallstudios.mhworlddatabase.getRouter
 import com.gatheringhallstudios.mhworlddatabase.util.BundleBuilder
 
 import java.util.ArrayList
 
 /**
- * Fragment for a list of monsters
+ * Fragment for a list of monster rewards
  */
-
 class MonsterRewardFragment : RecyclerViewFragment() {
     companion object {
-        private val ARG_MONSTER_ID = "MONSTER_ID"
         private val ARG_RANK = "RANK"
 
         @JvmStatic
@@ -36,23 +36,29 @@ class MonsterRewardFragment : RecyclerViewFragment() {
         }
     }
 
+    /**
+     * The viewmodel of the parent fragment (aka the monster)
+     */
     private val viewModel by lazy {
         ViewModelProviders.of(parentFragment!!).get(MonsterDetailViewModel::class.java)
     }
 
-    // todo: find a better way to have bundle null safety
-    private lateinit var rank : Rank
-    private lateinit var adapter: BasicListDelegationAdapter<Any>
+    /**
+     * The rank pertaining to this reward fragment
+     */
+    private var rank : Rank? = null
+
+    private val adapter = CategoryAdapter(
+            MonsterRewardAdapterDelegate({
+                getRouter().navigateItemDetail(it.data.item_id)
+            }))
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         // Setup Adapter to display rewards and headers
-        val rewardDelegate = MonsterRewardAdapterDelegate(::handleRewardSelection)
-        val subHeaderDelegate = SubHeaderAdapterDelegate()
-        adapter = BasicListDelegationAdapter(rewardDelegate, subHeaderDelegate)
         this.setAdapter(adapter)
 
         // Load data
-        rank = arguments!!.getSerializable(ARG_RANK) as Rank
+        rank = arguments?.getSerializable(ARG_RANK) as Rank?
         viewModel.rewards.observe(this, Observer(::setItems))
     }
 
@@ -61,31 +67,17 @@ class MonsterRewardFragment : RecyclerViewFragment() {
      * @param rewards items be of type Reward.
      */
     fun setItems(rewards: List<MonsterRewardView>?) {
+        adapter.clear()
         if (rewards == null) return
 
-        val grouped = rewards
-                .filter { it.data.rank == rank }
+        val grouped = rewards.asSequence()
+                .filter { it.data.rank == rank || rank == null }
                 .groupBy { it.condition_name }
 
-        val itemsWithHeaders = ArrayList<Any>()
         for ((condition, value) in grouped) {
-            itemsWithHeaders.add(SubHeader(condition))
-            itemsWithHeaders.addAll(value)
+            adapter.addSubSection(condition!!, value)
         }
 
-        adapter.items = itemsWithHeaders
         adapter.notifyDataSetChanged()
-    }
-
-    /**
-     * Handle onClick of rewards.
-     * @param object Object is guaranteed to be a Reward that was clicked
-     */
-    private fun handleRewardSelection(`object`: Any) {
-        val (_, _, item_name) = `object` as MonsterRewardView
-        Toast.makeText(context, "Clicked " + item_name!!, Toast.LENGTH_SHORT).show()
-        // TODO implement reward clicking to item details
-        //        Navigator nav = (Navigator)getActivity();
-        //        nav.navigateTo(MonsterDetailPagerFragment.getInstance(monster));
     }
 }
