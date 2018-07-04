f125366fa216a33a5951882bae40ada7f8c5eb2c
Carlos Fernandez <cfern1990@gmail.com>
2018-07-04 19:19:59 -0400

    Search: Log time and errors. Moved SearchFilter to dedicated file.

---
+++ app/src/main/java/com/gatheringhallstudios/mhworlddatabase/features/search/SearchFilter.kt
@@ -0,0 +1,41 @@
+package com.gatheringhallstudios.mhworlddatabase.features.search
+
+fun normalize(str: String): String {
+    // todo: make more efficient with a character to character mapping
+    // todo: support additional characters like greek ones, support locale transformations
+    var result = str.toLowerCase()
+    result = result.replace("α", "alpha")
+    result = result.replace("β", "beta")
+    return result
+}
+
+/**
+ * Class that accepts a filter string and matches it against test string.
+ */
+class SearchFilter(filterString: String) {
+    private val words = normalize(filterString).split(' ')
+
+    /**
+     * Checks if the filter string is a match for the text string after normalization.
+     */
+    fun matches(test: String?): Boolean {
+        test ?: return false
+        return matchesNormalized(normalize(test))
+    }
+
+    /**
+     * Checks if the filter string is a match for the test string without normalization.
+     * Use this if comparing to an already normalized test string.
+     */
+    fun matchesNormalized(test: String?): Boolean {
+        test ?: return false
+
+        for (word in words) {
+            if (!test.contains(word)) {
+                return false
+            }
+        }
+
+        return true
+    }
+}
--- app/src/main/java/com/gatheringhallstudios/mhworlddatabase/features/search/UniversalSearchViewModel.kt
+++ app/src/main/java/com/gatheringhallstudios/mhworlddatabase/features/search/UniversalSearchViewModel.kt
@@ -3,44 +3,20 @@ package com.gatheringhallstudios.mhworlddatabase.features.search
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.MutableLiveData
+import android.util.Log
 import com.gatheringhallstudios.mhworlddatabase.AppSettings
 import com.gatheringhallstudios.mhworlddatabase.common.CachedValue
 import com.gatheringhallstudios.mhworlddatabase.common.ThrottledExecutor
 import com.gatheringhallstudios.mhworlddatabase.data.MHWDatabase
 import com.gatheringhallstudios.mhworlddatabase.getResult
-
-fun normalize(str: String): String {
-    var result = str.toLowerCase()
-    result = result.replace("α", "alpha")
-    result = result.replace("β", "beta")
-    return result
-}
-
-class SearchFilter(private val filterString: String) {
-    private val words = normalize(filterString).split(' ')
-
-    fun matches(test: String?): Boolean {
-        test ?: return false
-        return matchesNormalized(normalize(test))
-    }
-
-    fun matchesNormalized(test: String?): Boolean {
-        test ?: return false
-
-        for (word in words) {
-            if (!test.contains(word)) {
-                return false
-            }
-        }
-
-        return true
-    }
-}
+import kotlin.system.measureTimeMillis
 
 // values are cached for 30 seconds
 val timeout: Long = 30 * 1000
 
 class UniversalSearchViewModel(app: Application) : AndroidViewModel(app) {
+    private val TAG = javaClass.simpleName
+
     private val db = MHWDatabase.getDatabase(app)
 
     // todo: if we ever need a more efficient search mechanism, implement a robust search dao or repo
@@ -78,19 +54,26 @@ class UniversalSearchViewModel(app: Application) : AndroidViewModel(app) {
         lastSearchFilter = trimmedString
 
         executor.execute {
-            if (trimmedString == "") {
-                searchResults.postValue(listOf())
-                return@execute
+            try {
+                val time = measureTimeMillis {
+                    handleSearch(trimmedString)
+                }
+
+                Log.d(TAG, "Search performed in $time milliseconds")
+            } catch (ex: Exception) {
+                Log.e(TAG, "Error while performing search", ex)
             }
-
-            val results = mutableListOf<Any>()
-
-            val filter = SearchFilter(trimmedString)
-            results.addAll(locationData.get().asSequence().filter { filter.matches(it.name) })
-            results.addAll(monsterData.get().asSequence().filter { filter.matches(it.name) })
-            results.addAll(itemData.get().asSequence().filter { filter.matches(it.name) })
-
-            searchResults.postValue(results)
         }
+    }
+
+    private fun handleSearch(filterStr: String) {
+        val results = mutableListOf<Any>()
+
+        val filter = SearchFilter(filterStr)
+        results.addAll(locationData.get().asSequence().filter { filter.matches(it.name) })
+        results.addAll(monsterData.get().asSequence().filter { filter.matches(it.name) })
+        results.addAll(itemData.get().asSequence().filter { filter.matches(it.name) })
+
+        searchResults.postValue(results)
     }
 }
