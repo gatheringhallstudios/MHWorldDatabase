0dcde6e5e5762d2a0a3c8ede2bb342aa0bacce38
Carlos Fernandez <cfern1990@gmail.com>
2018-07-11 22:24:01 -0400

    Armor: Styled and binded some data

--- app/src/main/java/com/gatheringhallstudios/mhworlddatabase/features/armor/ArmorSetAdapter.kt
+++ app/src/main/java/com/gatheringhallstudios/mhworlddatabase/features/armor/ArmorSetAdapter.kt
@@ -1,5 +1,7 @@
 package com.gatheringhallstudios.mhworlddatabase.features.armor
 
+import android.graphics.Color
+import android.support.v4.content.ContextCompat
 import com.gatheringhallstudios.mhworlddatabase.R
 import com.gatheringhallstudios.mhworlddatabase.assets.AssetLoader
 import com.gatheringhallstudios.mhworlddatabase.data.types.ArmorType
@@ -30,10 +32,23 @@ class ArmorSetHeaderItem(val armorSet: ArmorSetView) : Item(), ExpandableItem {
         val icon = loader.loadArmorIcon(ArmorType.CHEST, armorSet.armor.first().rarity)
         viewHolder.set_icon.setImageDrawable(icon)
         viewHolder.armor_set_name.text = armorSet.armorset_name
+        bindCurrentState(viewHolder)
 
         viewHolder.itemView.setOnClickListener {
             group.onToggleExpanded()
+            bindCurrentState(viewHolder)
         }
+    }
+
+    /**
+     * Updates view to match current expanded/collapsed state
+     */
+    private fun bindCurrentState(viewHolder: ViewHolder) {
+        val view = viewHolder.itemView
+        view.setBackgroundColor(when(group.isExpanded) {
+            true -> ContextCompat.getColor(view.context, R.color.backgroundColorSectionHeader)
+            false -> Color.TRANSPARENT
+        })
     }
 }
 
@@ -45,10 +60,22 @@ class ArmorSetDetailItem(val armor: ArmorView): Item() {
     override fun getLayout() = R.layout.listitem_armorset_armor
 
     override fun bind(viewHolder: ViewHolder, position: Int) {
+        val view = viewHolder.itemView
+
         viewHolder.armor_name.text = armor.name
+        viewHolder.rarity_string.text = view.resources.getString(R.string.rare_label, armor.rarity)
+        viewHolder.defense_value.text = view.resources.getString(
+                R.string.armor_defense_value,
+                armor.data.defense_base,
+                armor.data.defense_max,
+                armor.data.defense_augment_max)
 
         val loader = AssetLoader(viewHolder.itemView.context)
         val icon = loader.loadArmorIcon(armor.armor_type, armor.rarity)
         viewHolder.armor_icon.setImageDrawable(icon)
+
+        view.setOnClickListener {
+            // navigate
+        }
     }
 }
