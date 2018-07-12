02660e687e033e00fa78ccbacaa1ae7669c03f6e
Carlos Fernandez <cfern1990@gmail.com>
2018-07-11 22:44:09 -0400

    Armor: First attempt at header only item divider

--- app/src/main/java/com/gatheringhallstudios/mhworlddatabase/features/armor/ArmorSetListFragment.kt
+++ app/src/main/java/com/gatheringhallstudios/mhworlddatabase/features/armor/ArmorSetListFragment.kt
@@ -2,9 +2,10 @@ package com.gatheringhallstudios.mhworlddatabase.features.armor
 
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
+import android.graphics.Canvas
+import android.graphics.Rect
 import android.os.Bundle
-import android.support.v7.widget.DividerItemDecoration
-import android.support.v7.widget.DividerItemDecoration.VERTICAL
+import android.support.v7.widget.RecyclerView
 import android.view.View
 import com.gatheringhallstudios.mhworlddatabase.R
 import com.gatheringhallstudios.mhworlddatabase.common.RecyclerViewFragment
@@ -13,7 +14,50 @@ import com.gatheringhallstudios.mhworlddatabase.data.views.ArmorSetView
 import com.xwray.groupie.ExpandableGroup
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.ViewHolder
+import android.graphics.drawable.Drawable
+import android.support.v4.content.ContextCompat
+import com.xwray.groupie.ExpandableItem
 
+
+class HeaderItemDivider(private val mDivider: Drawable) : RecyclerView.ItemDecoration() {
+
+    fun childHasBorder(parent: RecyclerView, position: Int): Boolean {
+        val adapter = parent.adapter as GroupAdapter
+
+        val isAtEnd = position == (adapter.getItemCount() - 1)
+        val nextItem = when (isAtEnd) {
+                true -> null
+                false -> adapter.getItem(position + 1)
+        }
+
+        return nextItem is ExpandableItem
+    }
+
+    override fun onDraw(canvas: Canvas, parent: RecyclerView, state: RecyclerView.State?) {
+        canvas.save()
+
+        val left = parent.paddingLeft
+        val right = parent.width - parent.paddingRight
+
+        for (i in 0 until parent.childCount) {
+            val child = parent.getChildAt(i)
+            val trueIdx = parent.getChildAdapterPosition(child)
+
+            if (!childHasBorder(parent, trueIdx)) {
+                continue
+            }
+
+            val mBounds = Rect()
+            parent.getDecoratedBoundsWithMargins(child, mBounds)
+            val bottom = mBounds.bottom + Math.round(child.translationY)
+            val top = bottom - mDivider.intrinsicHeight
+
+            mDivider.setBounds(left, top, right, bottom)
+            mDivider.draw(canvas)
+        }
+        canvas.restore()
+    }
+}
 
 /**
  * Created by Carlos on 3/22/2018.
@@ -44,7 +88,8 @@ class ArmorSetListFragment : RecyclerViewFragment() {
         })
 
         // Add dividers between items
-        val itemDecor = DividerItemDecoration(context, VERTICAL)
+        val dividerDrawable = ContextCompat.getDrawable(context!!, R.drawable.listitem_divider)
+        val itemDecor = HeaderItemDivider(dividerDrawable!!)
         recyclerView.addItemDecoration(itemDecor)
     }
 
