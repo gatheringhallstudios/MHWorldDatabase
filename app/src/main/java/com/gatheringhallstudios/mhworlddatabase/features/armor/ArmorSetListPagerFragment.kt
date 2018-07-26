9079c158161102b21446facec95edd57990f7025
Carlos Fernandez <cfern1990@gmail.com>
2018-07-25 20:56:06 -0400

    Armor: Swapped High/Low rank tabs. High is first.

--- app/src/main/java/com/gatheringhallstudios/mhworlddatabase/features/armor/ArmorSetListPagerFragment.kt
+++ app/src/main/java/com/gatheringhallstudios/mhworlddatabase/features/armor/ArmorSetListPagerFragment.kt
@@ -6,13 +6,12 @@ import com.gatheringhallstudios.mhworlddatabase.data.types.Rank
 
 class ArmorSetListPagerFragment : BasePagerFragment() {
     override fun onAddTabs(tabs: TabAdder) {
-        tabs.addTab(getString(R.string.low_rank_full)) {
-            ArmorSetListFragment.newInstance(Rank.LOW)
-        }
-
         tabs.addTab(getString(R.string.high_rank_full)) {
             ArmorSetListFragment.newInstance(Rank.HIGH)
         }
-    }
 
+        tabs.addTab(getString(R.string.low_rank_full)) {
+            ArmorSetListFragment.newInstance(Rank.LOW)
+        }
+    }
 }
