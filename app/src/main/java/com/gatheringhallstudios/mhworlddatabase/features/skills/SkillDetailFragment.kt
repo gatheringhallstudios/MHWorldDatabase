11c16346bafc7a6b89b25d325370483b580d1e71
Aaron <ahctang@gmail.com>
2018-07-28 21:44:54 +0900

    Refactored the skill summary fragment to use the detail header cell

--- app/src/main/java/com/gatheringhallstudios/mhworlddatabase/features/skills/SkillDetailFragment.kt
+++ app/src/main/java/com/gatheringhallstudios/mhworlddatabase/features/skills/SkillDetailFragment.kt
@@ -47,10 +47,10 @@ class SkillDetailFragment : Fragment() {
         if (skillTreeFull == null) return
 
         val icon = assetLoader.loadIconFor(skillTreeFull)
-        skill_icon.setImageDrawable(icon)
-        skill_name.text = skillTreeFull.name
-        skill_description.text = skillTreeFull.description
-
+        skill_label.setIconDrawable(icon)
+        skill_label.setTitleText(skillTreeFull.name)
+        skill_label.setDescriptionText(skillTreeFull.description)
+        skill_label.removeDecorator()
         populateDescriptions(skillTreeFull.skills)
     }
 
