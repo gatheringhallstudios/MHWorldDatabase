d9b141a17401c0113e3164e2fe061d2f7dc336d0
Jayson Dela Cruz <jayson.p.delacruz@gmail.com>
2018-08-06 22:01:11 -0700

    Updated to DB-14.

--- app/src/main/java/com/gatheringhallstudios/mhworlddatabase/data/MHWDatabase.java
+++ app/src/main/java/com/gatheringhallstudios/mhworlddatabase/data/MHWDatabase.java
@@ -82,7 +82,7 @@ import com.gatheringhallstudios.mhworlddatabase.util.sqliteloader.SQLiteAssetHel
                 ArmorEntity.class, ArmorText.class, ArmorSkill.class, ArmorSetTextEntity.class,
                 ArmorSetBonusEntity.class, ArmorSetBonusTextEntity.class, ArmorRecipeEntity.class
         },
-        version = 13,
+        version = 14,
         exportSchema = false)
 @TypeConverters({Converters.class})
 public abstract class MHWDatabase extends RoomDatabase {
