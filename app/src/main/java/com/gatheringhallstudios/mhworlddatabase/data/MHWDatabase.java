94f5e2aa320779195953d1f71756a10065c2cccf
Carlos Fernandez <cfern1990@gmail.com>
2018-08-08 21:19:16 -0400

    DB Update to version 15

--- app/src/main/java/com/gatheringhallstudios/mhworlddatabase/data/MHWDatabase.java
+++ app/src/main/java/com/gatheringhallstudios/mhworlddatabase/data/MHWDatabase.java
@@ -82,7 +82,7 @@ import com.gatheringhallstudios.mhworlddatabase.util.sqliteloader.SQLiteAssetHel
                 ArmorEntity.class, ArmorText.class, ArmorSkill.class, ArmorSetTextEntity.class,
                 ArmorSetBonusEntity.class, ArmorSetBonusTextEntity.class, ArmorRecipeEntity.class
         },
-        version = 14,
+        version = 15,
         exportSchema = false)
 @TypeConverters({Converters.class})
 public abstract class MHWDatabase extends RoomDatabase {
