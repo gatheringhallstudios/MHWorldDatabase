81702b73fa0b130c5a156ec6393fac6f9a7d75f7
Aaron <ahctang@gmail.com>
2018-09-09 22:21:13 +0900

    Refactored the sharpness data so that it is handled in the model as opposed to in the sharpness view

--- app/src/main/java/com/gatheringhallstudios/mhworlddatabase/data/models/weapons.kt
+++ app/src/main/java/com/gatheringhallstudios/mhworlddatabase/data/models/weapons.kt
@@ -22,8 +22,6 @@ class WeaponTree(
         val element2: String?,
         val element2_attack: Int?,
         val element_hidden: Boolean,
-        val sharpness: String?,
-        val sharpness_maxed: Boolean,
         val defense: Int?,
         val previous_weapon_id: Int?
 ) : WeaponBase(id, name, weapon_type, rarity, attack) {
@@ -35,6 +33,8 @@ class WeaponTree(
     var formatter: MutableList<TreeFormatter> = mutableListOf()
     @Embedded
     lateinit var slots: WeaponSlots
+    @Embedded
+    var sharpnessData: WeaponSharpness? = null
 }
 
 /**
@@ -51,32 +51,76 @@ open class WeaponBase(
 )
 
 /**
- * An embedded class representing the available slots on a wepaon
+ * An embedded class representing the available slots on a weapon
  * Can be iterated on.
  */
 data class WeaponSlots(
         val slot_1: Int,
         val slot_2: Int,
         val slot_3: Int
-): Iterable<Int> {
+) : Iterable<Int> {
     /**
      * Returns a list containing only active (non-zero) slots
      */
-    @Ignore val active = this.asSequence().filter { it > 0 }.toList()
+    @Ignore
+    val active = this.asSequence().filter { it > 0 }.toList()
 
     /**
      * Returns true if the weapon contains slots, false otherwise
      */
-    @Ignore fun isEmpty() = active.isEmpty()
+    @Ignore
+    fun isEmpty() = active.isEmpty()
 
     override fun iterator(): Iterator<Int> {
         return listOf(slot_1, slot_2, slot_3).iterator()
     }
 
-    operator fun get(i: Int) = when(i) {
+    operator fun get(i: Int) = when (i) {
         0 -> slot_1
         1 -> slot_2
         2 -> slot_3
         else -> throw IndexOutOfBoundsException("Slot must be from 0-2")
+    }
+}
+
+/**
+ * An embedded class representing the sharpness on a weapon
+ */
+data class WeaponSharpness(
+        val sharpness_maxed: Boolean?,
+        val sharpness: String?
+) {
+    /**
+    Gets the sharpness of the weapon with the provided number of handicraft levels SUBTRACTED
+     */
+    fun get(handicraftLevel: Int): IntArray {
+        return adjustSharpness(handicraftLevel);
+    }
+
+    /**
+     * Function to adjust sharpness based on whether or not the weapon is affected by Handicraft
+     */
+    private fun adjustSharpness(handicraftLevel: Int): IntArray {
+        if (handicraftLevel > 5) throw IllegalArgumentException("Only a maximum of 5 handicraft levels can be subtracted")
+        if (sharpness == null || sharpness_maxed == null)  {
+            return IntArray(0)
+        }
+
+        val values = sharpness.split(",").map { it.toInt() }.toIntArray()
+        if (sharpness_maxed) return values
+
+        var index = values.size - 1
+        val handicraftBonus = 10 * handicraftLevel
+        var remainder: Int
+        do {
+            values[index] -= handicraftBonus
+            remainder = values[index]
+            if (values[index] < 0) {
+                values[index] = 0
+            }
+            index--
+        } while (remainder < 0 && index > 0)
+
+        return values
     }
 }
