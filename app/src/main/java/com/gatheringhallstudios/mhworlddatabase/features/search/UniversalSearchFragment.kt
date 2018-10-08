96dc88aad4692f95c04ae26290f748accc9fab7b
Carlos Fernandez <cfern1990@gmail.com>
2018-10-08 18:05:43 -0400

    Restore search results if activity has been destroyed
    We may or may not need to also restore scroll position but no idea how that works

--- app/src/main/java/com/gatheringhallstudios/mhworlddatabase/features/search/UniversalSearchFragment.kt
+++ app/src/main/java/com/gatheringhallstudios/mhworlddatabase/features/search/UniversalSearchFragment.kt
@@ -5,12 +5,11 @@ import android.os.Bundle
 import android.view.View
 import com.gatheringhallstudios.mhworlddatabase.MainActivity
 import com.gatheringhallstudios.mhworlddatabase.MainActivityViewModel
-import com.gatheringhallstudios.mhworlddatabase.adapters.common.BasicListDelegationAdapter
-import com.gatheringhallstudios.mhworlddatabase.adapters.SimpleUniversalBinderAdapterDelegate
 import com.gatheringhallstudios.mhworlddatabase.common.RecyclerViewFragment
 import com.gatheringhallstudios.mhworlddatabase.components.DashedDividerDrawable
 import com.gatheringhallstudios.mhworlddatabase.components.StandardDivider
 
+private const val SEARCH_FILTER = "SEARCH_FILTER"
 
 /**
  * The main fragment used to display search results
@@ -29,18 +28,27 @@ class UniversalSearchFragment : RecyclerViewFragment() {
         val adapter = SearchResultAdapter()
         setAdapter(adapter)
 
+        // If we need to restore state, start a query in the search view model.
+        // This will also update the current search filter, restoring the search title.
+        val savedFilter = savedInstanceState?.getString(SEARCH_FILTER)
+        if (!savedFilter.isNullOrBlank() && searchViewModel.searchFilter == "") {
+            searchViewModel.searchData(savedFilter!!)
+        }
+
         // add decorator
         recyclerView.addItemDecoration(StandardDivider(DashedDividerDrawable(context!!)))
 
         // open up the search menu (if not open) if we're on this page
         // If the user hit back and returned to this page, we need to open it again
         activityViewModel.searchActive.value = true
-        (activity as MainActivity).updateSearchView(searchViewModel.lastSearchFilter)
+        (activity as MainActivity).updateSearchView(searchViewModel.searchFilter)
 
+        // If the activity filter changes, update the fragment viewmodel
         activityViewModel.filter.observe(this, Observer {
             searchViewModel.searchData(it)
         })
 
+        // If the search results have a value or changed, show them.
         searchViewModel.searchResults.observe(this, Observer {
             if (it != null) adapter.bindSearchResults(it)
         })
@@ -56,5 +64,12 @@ class UniversalSearchFragment : RecyclerViewFragment() {
         super.onDestroyView()
 
         activityViewModel.searchActive.value = false
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        // save search filter to allow it to be restored
+        outState.putString(SEARCH_FILTER, searchViewModel.searchFilter)
     }
 }
