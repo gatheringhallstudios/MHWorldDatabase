0706f02a64ee7186058aa258f8e136ad479a597c
Carlos Fernandez <cfern1990@gmail.com>
2018-12-11 19:17:02 -0500

    Implemented "default" language setting, which uses the system locale.

--- app/src/main/java/com/gatheringhallstudios/mhworlddatabase/AppSettings.kt
+++ app/src/main/java/com/gatheringhallstudios/mhworlddatabase/AppSettings.kt
@@ -3,6 +3,7 @@ package com.gatheringhallstudios.mhworlddatabase
 import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
+import java.util.*
 
 /**
  * A static class used to manage shared preferences and application settings.
@@ -14,10 +15,19 @@ class AppSettings {
         val SETTINGS_FILE_NAME = "MHWDatabase.settings"
 
         private var application : Application? = null
+        private val validLanguages = mutableListOf<String>()
 
         @JvmStatic
         fun bindApplication(app : Application) {
             application = app
+        }
+
+        /**
+         * Binds the list of valid languages. Must be a list of locale ids.
+         */
+        @JvmStatic
+        fun bindValidLanguages(languages: List<String>) {
+            validLanguages.addAll(languages)
         }
 
         private val sharedPreferences : SharedPreferences
@@ -28,9 +38,41 @@ class AppSettings {
                 return application!!.applicationContext.getSharedPreferences(SETTINGS_FILE_NAME, Context.MODE_PRIVATE)
             }
 
+        /**
+         * Returns the configured data locale. This is the locale setting the user has set, which may be the empty string.
+         * When translating, use dataLocale instead, which returns the resolved data locale.
+         */
+        @JvmStatic
+        val configuredDataLocale: String
+            get() = sharedPreferences.getString(PROP_DATA_LOCALE, "") ?: ""
+
+        /**
+         * Returns the data locale that would be resolved
+         */
+        @JvmStatic
+        val defaultDataLocale: String
+            get() {
+                val locale = Locale.getDefault().language
+                return when (locale) {
+                    in validLanguages -> locale
+                    else -> "en"
+                }
+            }
+
+        /**
+         * Returns the resolved data locale. Defaults to the system language,
+         * or english if invalid.
+         */
         @JvmStatic
         val dataLocale : String
-            get() = sharedPreferences.getString(PROP_DATA_LOCALE, "en")
+            get() {
+                val pref = configuredDataLocale
+                if (pref.isNotBlank()) {
+                    return pref
+                }
+
+                return defaultDataLocale
+            }
 
         /**
          * Settings key for data locale language
