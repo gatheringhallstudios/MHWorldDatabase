5b80d91d99a870e0c6b8b6d09c19b8dc549097aa
Carlos Fernandez <cfern1990@gmail.com>
2019-03-03 21:05:20 -0500

    Weapon tree tab renamed from Regular to Normal

--- app/src/main/java/com/gatheringhallstudios/mhworlddatabase/features/weapons/list/WeaponTreePagerFragment.kt
+++ app/src/main/java/com/gatheringhallstudios/mhworlddatabase/features/weapons/list/WeaponTreePagerFragment.kt
@@ -1,23 +1,11 @@
 package com.gatheringhallstudios.mhworlddatabase.features.weapons.list
 
-import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
-import android.view.View
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.gatheringhallstudios.mhworlddatabase.R
 import com.gatheringhallstudios.mhworlddatabase.assets.AssetLoader
 import com.gatheringhallstudios.mhworlddatabase.common.BasePagerFragment
-import com.gatheringhallstudios.mhworlddatabase.common.RecyclerViewFragment
-import com.gatheringhallstudios.mhworlddatabase.components.DashedDividerDrawable
-import com.gatheringhallstudios.mhworlddatabase.components.StandardDivider
 import com.gatheringhallstudios.mhworlddatabase.data.types.WeaponType
-import com.gatheringhallstudios.mhworlddatabase.features.weapons.WeaponTreeAdapter
-import com.gatheringhallstudios.mhworlddatabase.getRouter
 import com.gatheringhallstudios.mhworlddatabase.setActivityTitle
-import com.gatheringhallstudios.mhworlddatabase.util.applyArguments
 
 /**
  * Fragment that displays the MHModelTree object.
@@ -39,7 +27,7 @@ class WeaponTreePagerFragment : BasePagerFragment() {
         // Load data
         viewModel.setWeaponType(type)
 
-        tabs.addTab(R.string.tab_weapon_regular) {
+        tabs.addTab(R.string.tab_weapon_normal) {
             WeaponTreeListFragment()
         }
 
